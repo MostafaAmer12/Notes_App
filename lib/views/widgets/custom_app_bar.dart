a109b7037de357f2a8bffc96b3f7b8eae5467270
MostafaAmer12 <61204848+MostafaAmer12@users.noreply.github.com>
2023-08-23 17:45:18 +0300

    Build Custom App Bar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_search_button.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('Notes',style: TextStyle(
+          fontSize: 28,
+        ),),
+        Spacer(),
+        CustomSearchButton(),
+      ],
+    );
+  }
+}
